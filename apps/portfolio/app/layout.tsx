c013758486faa40c0c667b9e294ceca79d957d84
ajay-8192 <ajaychowdary3699@gmail.com>
2025-09-20 12:24:11 +0530

    new changes

--- apps/portfolio/app/layout.tsx
+++ apps/portfolio/app/layout.tsx
@@ -49,8 +49,6 @@ export default function RootLayout({
         <link rel="manifest" href="/site.webmanifest" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='' />
-        <link rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
       </head>
       <body>{children}</body>
     </html>
