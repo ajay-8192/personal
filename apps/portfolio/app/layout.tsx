ad6f621462909fbbba1ab83239907de3b96d7cdd
ajay-8192 <ajaychowdary3699@gmail.com>
2025-09-20 19:23:09 +0530

    new changes

--- apps/portfolio/app/layout.tsx
+++ apps/portfolio/app/layout.tsx
@@ -47,8 +47,6 @@ export default function RootLayout({
         <link rel="shortcut icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="manifest" href="/site.webmanifest" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
       </head>
       <body>{children}</body>
     </html>
